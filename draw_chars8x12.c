9a1bfd39cfa40e9d380ed585b94b1a91170953bb
daniel <dacardenas5@miners.utep.edu>
2025-09-14 02:49:01 -0600

    finished font

--- draw_chars8x12.c
+++ draw_chars8x12.c
@@ -5,7 +5,7 @@ void print_char_8x12(char c)
 {
   c -= 0x20;
   for(char row = 0; row<8; row++){
-    for(char col = 0; col<12; col++){
+    for(char col = 11; col>=0; col--){
       unsigned short rowBits = font_8x12[c][col];
       unsigned short colMask = 1 << (7-row);
       putchar((rowBits & colMask) ? '*':' ');
