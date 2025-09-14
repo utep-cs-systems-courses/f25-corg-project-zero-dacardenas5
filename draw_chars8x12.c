17047e52bd97af3fe684b0f2ed1102f92180660c
daniel <dacardenas5@miners.utep.edu>
2025-09-14 02:21:57 -0600

    test

--- draw_chars8x12.c
+++ draw_chars8x12.c
@@ -4,13 +4,12 @@
 void print_char_8x12(char c)
 {
   c -= 0x20;
-  for(char col = 0; col<8; col++){
-    for(char row = 0; row<12; row++){
-      unsigned short rowBits = font_8x12[c][col];
-      unsigned short colMask = 1 << (11-row);
+  for(char col = 0; col<12; col++){
+    unsigned short rowBits = font_8x12[c][col];
+    for(char row = 0; row<8; row++){
+      unsigned short colMask = 1 << (7-col);
       putchar((rowBits & colMask) ? '*':' ');
     }
     putchar('\n');
   }
-  putchar('\n');
 }
